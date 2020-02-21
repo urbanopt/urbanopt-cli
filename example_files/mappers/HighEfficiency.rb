5bff66331262d5b79843359ddfe9600a02207723
Joe Robertson <joseph.robertson@nrel.gov>
2020-02-21 14:59:39 -0700

    Combine res and com paths into a single set of mappers.

--- example_files/mappers/HighEfficiency.rb
+++ example_files/mappers/HighEfficiency.rb
@@ -30,19 +30,33 @@
 
 require 'urbanopt/scenario'
 
-require_relative 'ResidentialBaseline'
+require_relative 'Baseline'
 
 require 'json'
 
 module URBANopt
   module Scenario
-    class ResidentialHighEfficiencyMapper < ResidentialBaselineMapper
+    class HighEfficiencyMapper < BaselineMapper
       
       def create_osw(scenario, features, feature_names)
       
         osw = super(scenario, features, feature_names)
 
-        # TODO
+        feature = features[0]
+        building_type = feature.building_type
+
+        if residential_building_types.include? building_type
+          # TODO
+        elsif commercial_building_types.include? building_type
+          OpenStudio::Extension.set_measure_argument(osw, 'IncreaseInsulationRValueForExteriorWalls', '__SKIP__', false)
+          OpenStudio::Extension.set_measure_argument(osw, 'IncreaseInsulationRValueForExteriorWalls', 'r_value', 20)
+
+          OpenStudio::Extension.set_measure_argument(osw, 'ReduceElectricEquipmentLoadsByPercentage', '__SKIP__', false)
+          OpenStudio::Extension.set_measure_argument(osw, 'ReduceElectricEquipmentLoadsByPercentage', 'elecequip_power_reduction_percent', 20)
+
+          OpenStudio::Extension.set_measure_argument(osw, 'ReduceLightingLoadsByPercentage', '__SKIP__', false)
+          OpenStudio::Extension.set_measure_argument(osw, 'ReduceLightingLoadsByPercentage', 'lighting_power_reduction_percent', 10)
+        end
 
         return osw
       end
