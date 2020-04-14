faf86e7755c3db71bd7e73f7957a90b85d0f9ab8
Nathan Moore <nathan.moore@nrel.gov>
2020-04-14 13:26:52 -0600

    Add tests for CLI funcionality

--- spec/uo_cli_spec.rb
+++ spec/uo_cli_spec.rb
@@ -1,8 +1,11 @@
 RSpec.describe URBANopt::CLI do
 
-  # FIXME: "warning: class variable access from toplevel" at every instance of this @@variable
-  @@test_directory = "spec/test_directory"
-  
+  test_directory = File.join("spec", "test_directory")
+  test_scenario = File.join(test_directory, "two_building_scenario.csv")
+  test_feature = File.join(test_directory, "example_project.json")
+  call_cli = "ruby #{File.join("lib", "uo_cli.rb")}"
+
+
   # Ensure clean slate for testing
   # +dir_or_file+ string - path to a file or folder
   def delete_directory_or_file(dir_or_file)
@@ -11,97 +14,128 @@ RSpec.describe URBANopt::CLI do
     end
   end
   
+  
   context "Admin" do
-    it 'has a version number' do
-      expect(URBANopt::CLI::VERSION).not_to be nil
+    it 'has the correct version number' do
+      # This test forces devs to come into the test suite when they've made changes to the cli
+      expect(URBANopt::CLI::VERSION).to eq("0.2.2")
     end
 
     it 'returns help' do
-      expect { system("ruby lib/uo_cli.rb -h") }
+      expect { system("#{call_cli} -h") }
         .to output(a_string_including('Usage: uo'))
         .to_stdout_from_any_process
+    end
+
+    it 'deletes a scenario' do
+      delete_directory_or_file(test_directory)
+      system("cp -R #{File.join("spec", "spec_files", "test_directory")} #{test_directory}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "1", "data_point_out.json"))).to be true
+      system("#{call_cli} -d -s #{test_scenario}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "1", "data_point_out.json"))).to be false
     end
   end
 
   context "Create project" do
     before :each do
-      delete_directory_or_file(@@test_directory)
+      delete_directory_or_file(test_directory)
     end
 
     it 'creates an example project directory' do
-      system("ruby lib/uo_cli.rb -p #{@@test_directory}")
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be true
+      system("#{call_cli} -p #{test_directory}")
+      expect(File.exists?(test_feature)).to be true
     end
 
     it 'creates an empty project directory' do
-      system("ruby lib/uo_cli.rb -e -p #{@@test_directory}")
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be false
-      expect(File.exists?(File.join(@@test_directory, "mappers", "Baseline.rb"))).to be true
+      system("#{call_cli} -e -p #{test_directory}")
+      expect(File.exists?(test_feature)).to be false
+      expect(File.exists?(File.join(test_directory, "mappers", "Baseline.rb"))).to be true
     end
 
     it 'does not overwrite a project directory without -o' do
-      system("ruby lib/uo_cli.rb -p #{@@test_directory}")
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be true
-      # FIXME: Test failure says nothing is written to stdout, but it is. Compare with line 20
-      expect { system("ruby lib/uo_cli.rb -p #{@@test_directory}") }
+      system("#{call_cli} -p #{test_directory}")
+      expect(File.exists?(test_feature)).to be true
+      expect { system("#{call_cli} -p #{test_directory}") }
         .to output(a_string_including('already a directory here'))
-        .to_stdout_from_any_process
+        .to_stderr_from_any_process
     end
 
     it "overwrites a project directory with -o" do
-      system("ruby lib/uo_cli.rb -p #{@@test_directory}")
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be true
-      # FIXME: Test failure says nothing is written to stdout, but it is. Compare with line 20
-      expect { system("ruby lib/uo_cli.rb -o -p #{@test_directory}") }
+      system("#{call_cli} -p #{test_directory}")
+      expect(File.exists?(test_feature)).to be true
+      expect { system("#{call_cli} -o -p #{test_directory}") }
         .to output(a_string_including("Overwriting"))
-        .to_stdout
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be true
+        .to_stdout_from_any_process
+      expect(File.exists?(test_feature)).to be true
     end
 
     it "overwrites an existing project directory with an empty directory" do
-      system("ruby lib/uo_cli.rb -p #{@@test_directory}")
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be true
-      system("ruby lib/uo_cli.rb -e -o -p #{@@test_directory}")
-      expect(File.exists?(File.join(@@test_directory, "example_project.json"))).to be false
-      expect(File.exists?(File.join(@@test_directory, "mappers", "Baseline.rb"))).to be true
+      system("#{call_cli} -p #{test_directory}")
+      expect(File.exists?(test_feature)).to be true
+      system("#{call_cli} -e -o -p #{test_directory}")
+      expect(File.exists?(test_feature)).to be false
+      expect(File.exists?(File.join(test_directory, "mappers", "Baseline.rb"))).to be true
     end
   end
 
-  context "Work with a small scenario" do
+  context "Prepare and run a small simulation" do
     before :all do
-      delete_directory_or_file(@@test_directory)
-      system("ruby lib/uo_cli.rb -p #{@@test_directory}")
+      delete_directory_or_file(test_directory)
+      system("#{call_cli} -p #{test_directory}")
     end
 
     it "creates a scenario file from a feature file" do
-      expect(File.exists?(File.join(@@test_directory, "baseline_scenario.csv"))).to be false
-      system("ruby lib/uo_cli.rb -m -f #{File.join(@@test_directory, "example_project.json")}")
-      expect(File.exists?(File.join(@@test_directory, "baseline_scenario.csv"))).to be true
+      expect(File.exists?(File.join(test_directory, "baseline_scenario.csv"))).to be false
+      system("#{call_cli} -m -f #{test_feature}")
+      expect(File.exists?(File.join(test_directory, "baseline_scenario.csv"))).to be true
+    end
+
+    it "creates a scenario file for a single feature from a feature file" do
+      system("uo -m -f #{test_feature} -i 1")
+      expect(File.exists?(File.join(test_directory, "baseline_scenario-1.csv"))).to be true
+    end
+
+    it "actually runs a 2 building scenario" do
+      # Copy in a scenario file with only the first 2 buildings in it
+      system("cp #{File.join("spec", "spec_files", "test_directory", "two_building_scenario.csv")} #{test_scenario}")
+      system("#{call_cli} -r -s #{test_scenario} -f #{test_feature}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "1", "finished.job"))).to be true
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "2", "finished.job"))).to be true
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "3", "finished.job"))).to be false
+    end
+  end
+
+  context "Post-process a scenario" do
+    before :all do
+      delete_directory_or_file(test_directory)
+      system("cp -R #{File.join("spec", "spec_files", "test_directory")} #{test_directory}")
     end
     
-    it "runs a 2 building scenario" do
-      # Copy in a scenario file with only the first 2 buildings in it
-      # TODO: Create the scenario file & edit it, so we don't need to maintain another copy that can get outdated
-      system("cp spec/spec_files/two_building_scenario.csv #{@@test_directory}/two_building_scenario.csv")
-      system("ruby lib/uo_cli.rb -r -s #{@@test_directory}/two_building_scenario.csv -f #{@@test_directory}/example_project.json")
-      expect(File.exists?(File.join(@@test_directory, "run", "two_building_scenario", "1", "finished.job"))).to be true
-      expect(File.exists?(File.join(@@test_directory, "run", "two_building_scenario", "2", "finished.job"))).to be true
-      expect(File.exists?(File.join(@@test_directory, "run", "two_building_scenario", "3", "finished.job"))).to be false
+    it "post-processor exits gracefully if given an invalid type" do
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "default_scenario_report.csv"))).to be false
+      expect { system("#{call_cli} -g -t foobar -s #{test_scenario} -f #{test_feature}") }
+        .to output(a_string_including("valid Gather type!"))
+        .to_stderr_from_any_process
     end
 
     it "post-processes a scenario" do
-      system("ruby lib/uo_cli.rb -g -t default -s #{@@test_directory}/two_building_scenario.csv -f #{@@test_directory}/example_project.json")
-      expect(File.exists?(File.join(@@test_directory, "run", "two_building_scenario", "default_scenario_report.csv"))).to be true
+      system("#{call_cli} -g -t default -s #{test_scenario} -f #{test_feature}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "default_scenario_report.csv"))).to be true
     end
 
-    # it "reopt post-processes for a whole scenario" do
-    #   system("ruby lib/uo_cli.rb -g -t reopt-scenario -s #{@@test_directory}/two_building_scenario.csv -f #{@@test_directory}/example_project.json")
-    # expect(File.exists?(File.join(@@test_directory, "run", "two_building_scenario", "reopt", "blah"))).to be true
-    # end
+    it "reopt post-processes a scenario" do
+      system("#{call_cli} -g -t reopt-scenario -s #{test_scenario} -f #{test_feature}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "reopt", "blah"))).to be true
+    end
 
-    # it "reopt post-processes each feature" do
-    #   system("ruby lib/uo_cli.rb -g -t reopt-feature -s #{@@test_directory}/two_building_scenario.csv -f #{@@test_directory}/example_project.json")
-    # expect(File.exists?(File.join(@@test_directory, "run", "two_building_scenario", "1", "blah"))).to be true
-    # end
+    it "reopt post-processes each feature" do
+      system("#{call_cli} -g -t reopt-feature -s #{test_scenario} -f #{test_feature}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "1", "blah"))).to be true
+    end
+
+    it "opendss post-processes a scenario" do
+      system("#{call_cli} -g -t opendss -s #{test_scenario} -f #{test_feature}")
+      expect(File.exists?(File.join(test_directory, "run", "two_building_scenario", "1", "feature_reports", "default_feature_report_opendss.csv"))).to be true
+    end
   end
 end
