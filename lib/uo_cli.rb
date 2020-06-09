9f47da311b96cea65a048fcf24705ba081b37357
Nathan Moore <nathan.moore@nrel.gov>
2020-06-09 14:48:57 -0600

    rubocop

--- lib/uo_cli.rb
+++ lib/uo_cli.rb
@@ -200,7 +200,7 @@ module URBANopt
       feature_file = URBANopt::GeoJSON::GeoFile.from_file(featurefile)
       if @opthash.subopts[:reopt] == true || @opthash.subopts[:reopt_scenario] == true || @opthash.subopts[:reopt_feature] == true
         # TODO: Better way of grabbing assumptions file than the first file in the folder
-        reopt_files_dir_contents_list = Dir.children("#{reopt_files_dir}")
+        reopt_files_dir_contents_list = Dir.children(reopt_files_dir.to_s)
         reopt_assumptions_filename = File.basename(reopt_files_dir_contents_list[0])
         scenario_output = URBANopt::Scenario::REoptScenarioCSV.new(name, @root_dir, run_dir, feature_file, mapper_files_dir, csv_file, num_header_rows, reopt_files_dir, reopt_assumptions_filename)
       else
