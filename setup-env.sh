f89292f05eb08000fd1f71d9af788322f7a2aa39
tijcolem <tjc.coleman@gmail.com>
2020-07-01 09:03:25 -0600

    add setup script

---
+++ setup-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+BASE_DIR_NAME=$(dirname `which $0`)
+
+GEM_HOME=${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0
+GEM_PATH=${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0
+PATH=${BASE_DIR_NAME}/ruby/bin:${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0/bin:$PATH
+RUBYLIB=${BASE_DIR_NAME}/OpenStudio/Ruby
+
+rm .env
+
+echo "export GEM_HOME=\"${GEM_HOME}\"" >> .env
+echo "export GEM_PATH=\"${GEM_PATH}\"" >> .env
+echo "export PATH=\"${PATH}\"" >> .env
+echo "export RUBYLIB=\"${RUBYLIB}\"" >> .env
+
+source .env
