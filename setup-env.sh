bd1a566f1fa2f02f28fb5ee3d900ef7d7b2e0cd4
tijcolem <tjc.coleman@gmail.com>
2020-07-23 01:08:20 +0000

    Update setup script

--- setup-env.sh
+++ setup-env.sh
@@ -6,7 +6,10 @@ GEM_PATH=${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0
 PATH=${BASE_DIR_NAME}/ruby/bin:${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0/bin:$PATH
 RUBYLIB=${BASE_DIR_NAME}/OpenStudio/Ruby
 
-rm .env
+# Remove it exists
+if [ -f .env ]; then
+  rm .env
+fi
 
 echo "export GEM_HOME=\"${GEM_HOME}\"" >> .env
 echo "export GEM_PATH=\"${GEM_PATH}\"" >> .env
