229db115eeec697f899db7501e3a332d1dc1e691
tijcolem <tjc.coleman@gmail.com>
2020-07-29 21:26:41 +0000

    Add in ruby dll env for windows

--- setup-env.sh
+++ setup-env.sh
@@ -1,19 +1,20 @@
 #!/bin/bash 
 BASE_DIR_NAME=$(dirname `which $0`)
 
-GEM_HOME=${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0
-GEM_PATH=${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0
-PATH=${BASE_DIR_NAME}/ruby/bin:${BASE_DIR_NAME}/urbanopt_cli_gems/ruby/2.5.0/bin:$PATH
+GEM_HOME=${BASE_DIR_NAME}/urbanopt_cli_gems
+GEM_PATH=${BASE_DIR_NAME}/urbanopt_cli_gems
+PATH=${BASE_DIR_NAME}/ruby/bin:${BASE_DIR_NAME}/urbanopt_cli_gems/bin:$PATH
 RUBYLIB=${BASE_DIR_NAME}/OpenStudio/Ruby
+RUBY_DLL_PATH=${BASE_DIR_NAME}/OpenStudio/Ruby
 
 # Remove it exists
-if [ -f .env ]; then
-  rm .env
+if [ -f env.sh ]; then
+  rm env.sh
 fi
 
-echo "export GEM_HOME=\"${GEM_HOME}\"" >> .env
-echo "export GEM_PATH=\"${GEM_PATH}\"" >> .env
-echo "export PATH=\"${PATH}\"" >> .env
-echo "export RUBYLIB=\"${RUBYLIB}\"" >> .env
+echo "export GEM_HOME=\"${GEM_HOME}\"" >> env.sh
+echo "export GEM_PATH=\"${GEM_PATH}\"" >> env.sh
+echo "export PATH=\"${PATH}\"" >> env.sh
+echo "export RUBYLIB=\"${RUBYLIB}\"" >> env.sh
+echo "export RUBY_DLL_PATH=\"${RUBY_DLL_PATH}\"" >> env.sh
 
-source .env
