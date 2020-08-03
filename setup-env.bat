d3a71bbf8dff5f067be83a17d12f6638b2a9c80c
tijcolem <tjc.coleman@gmail.com>
2020-08-03 22:47:23 +0000

    Add in windows .bat setup script

---
+++ setup-env.bat
@@ -0,0 +1,19 @@
+SET BASE_DIR_NAME=%cd%
+
+SET GEM_HOME=%BASE_DIR_NAME%\.gems
+SET GEM_PATH=%BASE_DIR_NAME%\.gems
+SET PATH=%BASE_DIR_NAME%\ruby\bin;%BASE_DIR_NAME%\urbanopt_cli_gems\bin;%PATH%
+SET RUBYLIB=%BASE_DIR_NAME%\OpenStudio\Ruby
+SET RUBY_DLL_PATH=%BASE_DIR_NAME%\OpenStudio\Ruby
+
+IF EXIST env.bat (
+  del env.bat
+) 
+
+echo SET GEM_HOME=%GEM_HOME%>> env.bat
+echo SET GEM_PATH=%GEM_PATH%>> env.bat
+echo SET PATH=%PATH%>> env.bat
+echo SET RUBYLIB=%RUBYLIB%>> env.bat
+echo SET RUBY_DLL_PATH=%RUBY_DLL_PATH%>> env.bat
+
+
