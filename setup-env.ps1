155d4b061fb101d24870c75fd443bde68dba512d
tijcolem <tjc.coleman@gmail.com>
2020-08-03 22:49:18 +0000

    change gem path

--- setup-env.ps1
+++ setup-env.ps1
@@ -6,16 +6,18 @@
 
 $BASE_DIR_NAME = $(Get-Location).Path
 
-$env:GEM_HOME = "$BASE_DIR_NAME\urbanopt_cli_gems"
-$env:GEM_PATH = "$BASE_DIR_NAME\urbanopt_cli_gems"
-$env:PATH += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\urbanopt_cli_gems\bin"
-$env:RUBYLIB = "$BASE_DIR_NAME\OpenStudio\Ruby"
+$env:GEM_HOME      = "$BASE_DIR_NAME\.gems"
+$env:GEM_PATH      = "$BASE_DIR_NAME\.gems"
+$env:PATH         += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\urbanopt_cli_gems\bin"
+$env:RUBYLIB       = "$BASE_DIR_NAME\OpenStudio\Ruby"
+$env:RUBY_DLL_PATH = "$BASE_DIR_NAME\OpenStudio\Ruby"
 
 # Remove if exists
 Remove-Item env.ps1 -ErrorAction Ignore
 
-'$env:GEM_HOME = "' + $env:GEM_HOME + '"'   >> env.ps1
-'$env:GEM_PATH = "' + $env:GEM_PATH + '"'   >> env.ps1
-'$env:PATH     = "' + $env:PATH     + '"'   >> env.ps1
-'$env:RUBYLIB  = "' + $env:RUBYLIB  + '"'   >> env.ps1
+'$env:GEM_HOME       = "' + $env:GEM_HOME + '"'   >> env.ps1
+'$env:GEM_PATH       = "' + $env:GEM_PATH + '"'   >> env.ps1
+'$env:PATH           = "' + $env:PATH     + '"'   >> env.ps1
+'$env:RUBYLIB        = "' + $env:RUBYLIB  + '"'   >> env.ps1
+'$env:RUBY_DLL_PATH  = "' + $env:RUBY_DLL_PATH  + '"'   >> env.ps1
 
