212a86f7dfbc76f844ab1e0f915a10e53d5a9aca
Tim Coleman <tjc.coleman@gmail.com>
2020-08-20 18:15:53 -0600

    Update unix setup to write env to home dir

--- setup-env-unix.sh
+++ setup-env-unix.sh
@@ -14,14 +14,14 @@ RUBYLIB=${BASE_DIR_NAME}/OpenStudio/Ruby
 RUBY_DLL_PATH=${BASE_DIR_NAME}/OpenStudio/Ruby
 
 # Remove if exists
-if [ -f env.sh ]; then
-  rm env.sh
+if [ -f ~/.env_uo.sh ]; then
+  rm ~/.env_uo.sh
 fi
 
-echo "export GEM_HOME=\"${GEM_HOME}\"" >> env.sh
-echo "export GEM_PATH=\"${GEM_PATH}\"" >> env.sh
-echo "export PATH=\"${PATH}\"" >> env.sh
-echo "export RUBYLIB=\"${RUBYLIB}\"" >> env.sh
-echo "export RUBY_DLL_PATH=\"${RUBY_DLL_PATH}\"" >> env.sh
+echo "export GEM_HOME=\"${GEM_HOME}\"" >> ~/.env_uo.sh
+echo "export GEM_PATH=\"${GEM_PATH}\"" >> ~/.env_uo.sh
+echo "export PATH=\"${PATH}\"" >> ~/.env_uo.sh
+echo "export RUBYLIB=\"${RUBYLIB}\"" >> ~/.env_uo.sh
+echo "export RUBY_DLL_PATH=\"${RUBY_DLL_PATH}\"" >> ~/.env_uo.sh
 
 
