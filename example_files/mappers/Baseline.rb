712487e9dc29a8ada7378522f90c0a89e0a042f9
kflemin <katherine.fleming@nrel.gov>
2020-09-13 15:18:53 -0600

    adding tm symbol to files

--- example_files/mappers/Baseline.rb
+++ example_files/mappers/Baseline.rb
@@ -1,5 +1,5 @@
 #*********************************************************************************
-# URBANopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
+# URBANopt™, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
