6bd15f445efc2098a286d42f01d7de7e905b8630
tijcolem <tjc.coleman@gmail.com>
2020-11-23 16:42:43 -0700

    use HOMEPATH vs HOME in bat setup script

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -9,14 +9,14 @@ SET PATH=%BASE_DIR_NAME%\ruby\bin;%BASE_DIR_NAME%\gems\ruby\2.5.0\bin;%PATH%
 SET RUBYLIB=%BASE_DIR_NAME%\OpenStudio\Ruby
 SET RUBY_DLL_PATH=%BASE_DIR_NAME%\OpenStudio\Ruby
 
-IF EXIST %HOME%\.env_uo.bat (
-  del %HOME%\.env_uo.bat
+IF EXIST %HOMEPATH%\.env_uo.bat (
+  del %HOMEPATH%\.env_uo.bat
 ) 
 
-echo SET GEM_HOME=%GEM_HOME%>> %HOME%\.env_uo.bat
-echo SET GEM_PATH=%GEM_PATH%>> %HOME%\.env_uo.bat
-echo SET PATH=%PATH%>> %HOME%\.env_uo.bat
-echo SET RUBYLIB=%RUBYLIB%>> %HOME%\.env_uo.bat
-echo SET RUBY_DLL_PATH=%RUBY_DLL_PATH%>> %HOME%\.env_uo.bat
+echo SET GEM_HOME=%GEM_HOME%>> %HOMEPATH%\.env_uo.bat
+echo SET GEM_PATH=%GEM_PATH%>> %HOMEPATH%\.env_uo.bat
+echo SET PATH=%PATH%>> %HOMEPATH%\.env_uo.bat
+echo SET RUBYLIB=%RUBYLIB%>> %HOMEPATH%\.env_uo.bat
+echo SET RUBY_DLL_PATH=%RUBY_DLL_PATH%>> %HOMEPATH%\.env_uo.bat
 
 
