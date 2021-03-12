baf0bedc514883b2bae8899f393420c346bd9709
Nathan Moore <nathan.moore@nrel.gov>
2021-03-12 14:05:20 -0700

    update license & copyright

--- example_files/mappers/Baseline.rb
+++ example_files/mappers/Baseline.rb
@@ -1,21 +1,31 @@
 #*********************************************************************************
-# URBANopt™, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
+# URBANopt™, Copyright (c) 2019-2021, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
-#
+
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
-#
+
 # Redistributions of source code must retain the above copyright notice, this list
 # of conditions and the following disclaimer.
-#
+
 # Redistributions in binary form must reproduce the above copyright notice, this
 # list of conditions and the following disclaimer in the documentation and/or other
 # materials provided with the distribution.
-#
+
 # Neither the name of the copyright holder nor the names of its contributors may be
 # used to endorse or promote products derived from this software without specific
 # prior written permission.
-#
+
+# Redistribution of this software, without modification, must refer to the software
+# by the same designation. Redistribution of a modified version of this software
+# (i) may not refer to the modified version by the same designation, or by any
+# confusingly similar designation, and (ii) must refer to the underlying software
+# originally provided by Alliance as “URBANopt”. Except to comply with the foregoing,
+# the term “URBANopt”, or any confusingly similar designation may not be used to
+# refer to any modified version of this software or any modified version of the
+# underlying software originally provided by Alliance without the prior written
+# consent of Alliance.
+
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -271,7 +281,7 @@ module URBANopt
           'Food service',
           'Inpatient health care',
           'Nursing',
-          'Lodging',          
+          'Lodging',
           'Strip shopping mall',
           'Enclosed mall',
           'Retail other than mall',
@@ -383,7 +393,7 @@ module URBANopt
       end
 
       def create_osw(scenario, features, feature_names)
-        
+
         if features.size != 1
           raise "Baseline currently cannot simulate more than one feature."
         end
@@ -947,10 +957,10 @@ module URBANopt
                 end
               rescue
               end
-              
+
               # TODO: surface_elevation has no current mapping
               # TODO: tariff_filename has no current mapping
-              
+
               # create a bar building, will have spaces tagged with individual space types given the
               # input building types
               # set skip measure to false
