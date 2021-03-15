2a4fbbfd47703995bcc535e73ed85a7ea772d911
Nathan Moore <nathan.moore@nrel.gov>
2021-03-15 11:56:02 -0600

    update license & copyright (#208)

--- example_files/mappers/EvCharging.rb
+++ example_files/mappers/EvCharging.rb
@@ -1,21 +1,31 @@
 # *********************************************************************************
-# URBANopt™, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
+# URBANopt™, Copyright (c) 2019-2021, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
-#
+
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
-#
+
 # Redistributions of source code must retain the above copyright notice, this list
 # of conditions and the following disclaimer.
-#
+
 # Redistributions in binary form must reproduce the above copyright notice, this
 # list of conditions and the following disclaimer in the documentation and/or other
 # materials provided with the distribution.
-#
+
 # Neither the name of the copyright holder nor the names of its contributors may be
 # used to endorse or promote products derived from this software without specific
 # prior written permission.
-#
+
+# Redistribution of this software, without modification, must refer to the software
+# by the same designation. Redistribution of a modified version of this software
+# (i) may not refer to the modified version by the same designation, or by any
+# confusingly similar designation, and (ii) must refer to the underlying software
+# originally provided by Alliance as “URBANopt”. Except to comply with the foregoing,
+# the term “URBANopt”, or any confusingly similar designation may not be used to
+# refer to any modified version of this software or any modified version of the
+# underlying software originally provided by Alliance without the prior written
+# consent of Alliance.
+
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -41,14 +51,14 @@ module URBANopt
     class EvChargingMapper < HighEfficiencyMapper
       def create_osw(scenario, features, feature_names)
         osw = super(scenario, features, feature_names)
-        
+
         feature = features[0]
 
         def ev_charging_type(building_type)
           typical_home = ['Single-Family', 'Multifamily (2 to 4 units)', 'Multifamily (5 or more units)', 'Lodging']
           typical_public = ['Public assembly', 'Strip shopping mall', 'Enclosed mall', 'Retail other than mall', 'Food service', 'Nonrefrigerated warehouse', 'Food sales', 'Refrigerated warehouse', 'Religious worship', 'Service', 'Public order and safety', 'Uncovered Parking', 'Covered Parking']
           typical_work = ['Office', 'Laboratory', 'Education', 'Inpatient health care', 'Outpatient health care', 'Nursing']
-  
+
           if typical_home.include? building_type
             return 'Typical Home'
           elsif typical_public.include? building_type
@@ -57,7 +67,7 @@ module URBANopt
             return 'Typical Work'
           end
         end
-  
+
         # add EV loads
         ev_charging = nil
 
@@ -65,7 +75,7 @@ module URBANopt
           ev_charging = feature.ev_charging
         rescue
         end
-          
+
         if ev_charging != true
           puts "Please set ev_charging to true to add EV loads."
         elsif ev_charging == true
