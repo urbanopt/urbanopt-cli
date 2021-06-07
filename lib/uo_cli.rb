4ba54b83b0699951c98fd247361d93d9acf324df
Nathan Moore <nathan.moore@nrel.gov>
2021-06-07 09:44:48 -0600

    clean up code display for developers

--- lib/uo_cli.rb
+++ lib/uo_cli.rb
@@ -854,15 +854,29 @@ module URBANopt
           scenario_assumptions = File.expand_path(@opthash.subopts[:reopt_scenario_assumptions_file]).to_s
         end
         puts "\nRunning the REopt Scenario post-processor with scenario assumptions file: #{scenario_assumptions}\n"
-        reopt_post_processor = URBANopt::REopt::REoptPostProcessor.new(scenario_report, scenario_assumptions, scenario_base.reopt_feature_assumptions, DEVELOPER_NREL_KEY)
+        reopt_post_processor = URBANopt::REopt::REoptPostProcessor.new(
+          scenario_report,
+          scenario_assumptions,
+          scenario_base.reopt_feature_assumptions,
+          DEVELOPER_NREL_KEY
+        )
         if @opthash.subopts[:reopt_scenario] == true
           puts "\nPost-processing entire scenario with REopt\n"
-          scenario_report_scenario = reopt_post_processor.run_scenario_report(scenario_report: scenario_report, save_name: 'scenario_optimization', run_resilience: @opthash.subopts[:reopt_resilience])
+          scenario_report_scenario = reopt_post_processor.run_scenario_report(
+            scenario_report: scenario_report,
+            save_name: 'scenario_optimization',
+            run_resilience: @opthash.subopts[:reopt_resilience]
+          )
           results << { "process_type": 'reopt_scenario', "status": 'Complete', "timestamp": Time.now.strftime('%Y-%m-%dT%k:%M:%S.%L') }
           puts "\nDone\n"
         elsif @opthash.subopts[:reopt_feature] == true
           puts "\nPost-processing each building individually with REopt\n"
-            scenario_report_features = reopt_post_processor.run_scenario_report_features(scenario_report: scenario_report, save_names_feature_reports: ['feature_optimization'] * scenario_report.feature_reports.length, save_name_scenario_report: 'feature_optimization', run_resilience: @opthash.subopts[:reopt_resilience], keep_existing_output: @opthash.subopts[:reopt_keep_existing])
+            scenario_report_features = reopt_post_processor.run_scenario_report_features(
+              scenario_report: scenario_report,
+              save_names_feature_reports: ['feature_optimization'] * scenario_report.feature_reports.length,
+              save_name_scenario_report: 'feature_optimization',
+              run_resilience: @opthash.subopts[:reopt_resilience]
+            )
           results << { "process_type": 'reopt_feature', "status": 'Complete', "timestamp": Time.now.strftime('%Y-%m-%dT%k:%M:%S.%L') }
           puts "\nDone\n"
         end
