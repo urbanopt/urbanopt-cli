56fc8d3cced5adcd10393d473e1916fa5c07e6b0
Katherine Fleming <katherine.fleming@nrel.gov>
2022-03-16 15:09:54 -0600

    updating copyrights for 2022 (#319)

--- lib/uo_cli/version.rb
+++ lib/uo_cli/version.rb
@@ -1,5 +1,5 @@
 # *********************************************************************************
-# URBANopt™, Copyright (c) 2019-2021, Alliance for Sustainable Energy, LLC, and other
+# URBANopt™, Copyright (c) 2019-2022, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without modification,
