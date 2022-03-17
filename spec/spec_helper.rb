4d1f1ca799f245f461481e45640e0be67348bd35
Nathan Moore <nathan.moore@nrel.gov>
2022-03-17 11:39:29 -0600

    Revert "Revert "[skip ci] Another tiny comment change""
    This reverts commit 186672b548f6eec807530a682733f0483c8cb16c.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@
 require 'bundler/setup'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
+  # Recording test status enables flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = 'spec/.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
