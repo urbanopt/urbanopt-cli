00c45df9967fe4222428233afd1eb2e23ce841d3
Nathan Moore <nathan.moore@nrel.gov>
2023-01-04 13:36:17 -0700

    different syntax for coveralls filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,7 +45,7 @@ SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 # Don't consider the spec folder for test coverage reporting (inside the do/end loop)
 SimpleCov.start do
-  add_filter '/spec/'
+  add_filter './spec/'
 end
 
 require 'bundler/setup'
