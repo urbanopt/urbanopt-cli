f870e1ac5a2fe1ca0f118e8338850ffe21c76247
Nathan Moore <nathan.moore@nrel.gov>
2024-10-02 15:10:19 -0600

    remove hard-coded version string from env setup

--- scripts/setup-env.ps1
+++ scripts/setup-env.ps1
@@ -1,17 +1,18 @@
-# This is a simple setup script that generates an enviroment file that
-# is used to setup the ruby enviroment to run the urbanopt-cli tool.
+# This is a simple setup script that generates an environment file that
+# is used to setup the ruby environment to run the urbanopt-cli tool.
 # To use just run this script in powershell (e.g. ./setup-env.ps1)
-# Then you can use this env.ps1 to setup the enviroment.
+# Then you can use this env.ps1 to setup the environment.
 # (e.g. . env.ps1)
 
 if (-not (Test-Path $HOME)) { echo "env HOME needs to be set before running this script" }
 if (-not (Test-Path $HOME)) { exit }
 
 $BASE_DIR_NAME = $PSScriptRoot
+UO_DIR=Split-Path -Path $PSScriptRoot -Leaf
 
 $env:GEM_HOME      = "$BASE_DIR_NAME\gems\ruby\2.7.0"
 $env:GEM_PATH      = "$BASE_DIR_NAME\gems\ruby\2.7.0"
-$env:PATH         += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\gems\urbanopt-cli-0.11.1\example_files\python_deps\python-3.10"
+$env:PATH         += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\gems\$UO_DIR\example_files\python_deps\python-3.10"
 $env:RUBYLIB       = "$BASE_DIR_NAME\OpenStudio\Ruby"
 $env:RUBY_DLL_PATH = "$BASE_DIR_NAME\OpenStudio\Ruby"
 
