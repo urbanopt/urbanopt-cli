f6c34022c77614384c9620a9b97b8b487c7618c1
tijcolem <tjc.coleman@gmail.com>
2024-10-10 11:31:27 -0600

    Find the python path if it exists in urbanopt gem directories

--- scripts/setup-env.ps1
+++ scripts/setup-env.ps1
@@ -7,12 +7,22 @@
 if (-not (Test-Path $HOME)) { echo "env HOME needs to be set before running this script" }
 if (-not (Test-Path $HOME)) { exit }
 
+# uo install_python will install its own python within the gem directories so we need to find the python path and add it to $env.PATH
+$output = Get-ChildItem -ErrorAction SilentlyContinue -Directory "C:\URBANopt*" -Recurse -Filter "python-3.10" | Select-Object FullName
+
+if ($output.FullName) { 
+  $RUBY_PYTHON_PATH = $output.FullName 
+}
+else {
+  $RUBY_PYTHON_PATH = ""
+}
+
+
 $BASE_DIR_NAME = $PSScriptRoot
-$UO_DIR = Split-Path -Path $PSScriptRoot -Leaf
 
 $env:GEM_HOME      = "$BASE_DIR_NAME\gems\ruby\2.7.0"
 $env:GEM_PATH      = "$BASE_DIR_NAME\gems\ruby\2.7.0"
-$env:PATH         += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\gems\$UO_DIR\example_files\python_deps\python-3.10"
+$env:PATH         += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\gems\ruby\2.7.0\bin;$RUBY_PYTHON_PATH"
 $env:RUBYLIB       = "$BASE_DIR_NAME\OpenStudio\Ruby"
 $env:RUBY_DLL_PATH = "$BASE_DIR_NAME\OpenStudio\Ruby"
 
@@ -23,4 +33,4 @@ Remove-Item $HOME/.env_uo.ps1 -ErrorAction Ignore
 '$env:GEM_PATH       = "' + $env:GEM_PATH + '"'   >> $HOME/.env_uo.ps1
 '$env:PATH           = "' + $env:PATH     + '"'   >> $HOME/.env_uo.ps1
 '$env:RUBYLIB        = "' + $env:RUBYLIB  + '"'   >> $HOME/.env_uo.ps1
-'$env:RUBY_DLL_PATH  = "' + $env:RUBY_DLL_PATH  + '"'   >> $HOME/.env_uo.ps1
+'$env:RUBY_DLL_PATH  = "' + $env:RUBY_DLL_PATH  + '"'   >> $HOME/.env_uo.ps1 
