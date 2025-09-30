5606e2fe4ac3541b806e474a8f97d067e1b2d651
Nathan Moore <nathan.moore@nrel.gov>
2025-09-30 11:23:28 -0600

    clean up and comment my formatting utility function

--- lib/uo_cli/utils.rb
+++ lib/uo_cli/utils.rb
@@ -1,4 +1,4 @@
 def format_float(num)
-  s = '%.2f' % num
-  float_as_string = s.gsub(/(\d)(?=(\d{3})+\.)/, '\1,')
+  s = "#{num.round(2)}"
+  float_as_string = s.gsub(/(\d)(?=(\d{3})+\.)/, '\1,') # add commas as thousands separators
 end
