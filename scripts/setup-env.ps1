4ecc54fcef0a33ab311335b2191cdac67b920836
tijcolem <tjc.coleman@gmail.com>
2026-02-26 10:08:33 -0700

    gem updates and adding openstudo gem package for windows runtime

--- scripts/setup-env.ps1
+++ scripts/setup-env.ps1
@@ -25,8 +25,8 @@ $BASE_DIR_NAME = $PSScriptRoot
 
 $GEM_HOME      = "$BASE_DIR_NAME\gems\ruby\$RUBY_BASE_VERSION"
 $GEM_PATH      = "$BASE_DIR_NAME\gems\ruby\$RUBY_BASE_VERSION"
-$UO_GEMFILE_PATH  = "$UO_DIR\gems\Gemfile"
-$UO_BUNDLE_INSTALL_PATH  = "$UO_DIR\gems"
+$UO_GEMFILE_PATH  = "$UO_DIR\openstudio-runtime-gems\Gemfile"
+$UO_BUNDLE_INSTALL_PATH  = "$UO_DIR\openstudio-runtime-gems"
 $PATH         += ";$BASE_DIR_NAME\ruby\bin;$BASE_DIR_NAME\gems\ruby\$RUBY_BASE_VERSION\bin;$RUBY_PYTHON_PATH;$env::PATH"
 $RUBYLIB       = "$BASE_DIR_NAME\OpenStudio\Ruby"
 $RUBY_DLL_PATH = "$BASE_DIR_NAME\OpenStudio\Ruby"
